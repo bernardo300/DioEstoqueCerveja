f034e9d3eac261fd14e8eafb7905cf00a2823300
bernardo300 <bernardolopes.luz@hotmail.com>
2021-07-06 22:42:54 -0300

    firt commit

---
+++ src/main/java/dio/DioEstoqueCerveja/DioEstoqueCervejaApplication.java
@@ -0,0 +1,13 @@
+package dio.DioEstoqueCerveja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioEstoqueCervejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioEstoqueCervejaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/DioEstoqueCerveja/DioEstoqueCervejaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.DioEstoqueCerveja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioEstoqueCervejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
